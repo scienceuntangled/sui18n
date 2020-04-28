87b9137ab321b2d3968882d0fdef9189aa52a49f
Ben Raymond <ben@theraymonds.org>
2020-04-28 21:03:35 +1000

    read csv with known encoding v0.1.1

--- R/translate.R
+++ R/translate.R
@@ -28,7 +28,7 @@ sui_translator <- function(to, csv_path = system.file("extdata/su_translations.c
     if (is.data.frame(csv_path)) {
         tdata <- csv_path
     } else {
-        tdata <- read.csv(csv_path, stringsAsFactors = FALSE, comment.char = "@")
+        tdata <- read.csv(csv_path, stringsAsFactors = FALSE, comment.char = "@", encoding = "UTF-8")
         tdata <- tdata[, !grepl("^X", colnames(tdata))] ## drop unnamed cols
     }
     for (ci in seq_len(ncol(tdata))) {
