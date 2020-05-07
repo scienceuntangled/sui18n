ab6e92d45dc5ed2421383159e87fe2cc8f7350cb
Ben Raymond <ben@theraymonds.org>
2020-05-07 17:53:13 +1000

    comments, fix regexps

--- R/internal_utils.R
+++ R/internal_utils.R
@@ -14,19 +14,19 @@ is_firstupper <- function(z) {
     grepl("[[:upper:]]", substr(z, 1, 1)) && !grepl("[[:upper:]]", substr(z, 2, nchar(z)))
 }
 
-
+## internal functions to convert string to various case types, but ignoring any leading non-alpha chars like punctuation
 punct_str_to_upper <- function(x, locale) {
-    leading_punct <- str_match(x, "^([^[:alnum:]]*)")[1, 1]
-    paste0(leading_punct, str_to_upper(sub("^([^[:alnum:]]*)", "", x), locale = locale))
+    leading_punct <- str_match(x, "^([^[:alpha:]]*)")[1, 1]
+    paste0(leading_punct, str_to_upper(sub("^([^[:alpha:]]*)", "", x), locale = locale))
 }
 
 punct_str_to_title <- function(x, locale) {
-    leading_punct <- str_match(x, "^([^[:alnum:]]*)")[1, 1]
-    paste0(leading_punct, str_to_title(sub("^([^[:alnum:]]*)", "", x), locale = locale))
+    leading_punct <- str_match(x, "^([^[:alpha:]]*)")[1, 1]
+    paste0(leading_punct, str_to_title(sub("^([^[:alpha:]]*)", "", x), locale = locale))
 }
 
 punct_str_to_firstupper <- function(x, locale) {
-    leading_punct <- str_match(x, "^([^[:alnum:]]*)")[1, 1]
-    x <- sub("^([^[:alnum:]]*)", "", x)
+    leading_punct <- str_match(x, "^([^[:alpha:]]*)")[1, 1]
+    x <- sub("^([^[:alpha:]]*)", "", x)
     paste0(leading_punct, str_to_upper(substr(x, 1, 1), locale = locale), str_to_lower(substr(x, 2, nchar(x)), locale = locale))
 }
