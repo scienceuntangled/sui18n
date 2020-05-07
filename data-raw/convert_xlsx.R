cda7fdf45c1314629aad9ad8af2a44f217fe5eb0
Ben Raymond <ben@theraymonds.org>
2020-05-07 17:54:11 +1000

    add spanish, other fixes v0.2.0

--- data-raw/convert_xlsx.R
+++ data-raw/convert_xlsx.R
@@ -21,3 +21,45 @@ for (l in seq_len(nrow(x))) {
     }
 }
 close(con)
+
+
+## checks
+source("../R/internal_utils.R")
+library(stringr)
+x <- read.csv("../inst/extdata/su_translations.csv", stringsAsFactors = FALSE, comment.char = "@", encoding = "UTF-8")
+stopifnot(!any(grepl("^X", colnames(x)))) ## all cols named
+en_uppercase <- unlist(lapply(x$en, is_uppercase))
+en_titlecase <- unlist(lapply(x$en, is_titlecase))
+en_firstupper <- unlist(lapply(x$en, is_firstupper))
+for (tgt in setdiff(colnames(x), c("en", "key"))) {
+    check_uppercase <- unlist(lapply(x[[tgt]], is_uppercase))
+    check_titlecase <- unlist(lapply(x[[tgt]], is_titlecase))
+    check_firstupper <- unlist(lapply(x[[tgt]], is_firstupper))
+    chk <- check_uppercase != en_uppercase | check_titlecase != en_titlecase | check_firstupper != en_firstupper
+    chk <- chk | (grepl("[A-Z]", substr(x$en, 1, 1)) & grepl("[a-z]", substr(x[[tgt]], 1, 1))) | (grepl("[a-z]", substr(x$en, 1, 1)) & grepl("[A-Z]", substr(x[[tgt]], 1, 1)))
+    if (any(chk)) {
+        cat("Language ", tgt, " case comparisons need checking:\n", sep = "")
+        for (ii in which(chk)) {
+            cat("[", ii, "] en: ", x$en[ii], "\n[", ii, "] ", tgt, ": ", x[[tgt]][ii], "\n\n", sep = "")
+        }
+    } else {
+        cat("Language ", tgt, " case comparisons all passed.\n", sep = "")
+    }
+
+    chk <- is.na(x[[tgt]]) | x$en==x[[tgt]]
+    if (any(chk)) {
+        cat("Language ", tgt, " identical entries need checking:\n", sep = "")
+        for (ii in which(chk)) {
+            cat("[", ii, "] en: ", x$en[ii], "\n[", ii, "] ", tgt, ": ", x[[tgt]][ii], "\n\n", sep = "")
+        }
+    }
+
+    chk <- grepl("\"", x[[tgt]])
+    if (any(chk)) {
+        cat("Language ", tgt, " has embedded double quotes:\n", sep = "")
+        for (ii in which(chk)) {
+            cat("[", ii, "] en: ", x$en[ii], "\n[", ii, "] ", tgt, ": ", x[[tgt]][ii], "\n\n", sep = "")
+        }
+    }
+}
+
